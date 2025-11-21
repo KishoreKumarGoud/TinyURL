d7664df5d18afa8d625897c055e9fa4ab7ad6d2d
datamarathon <datamarathonhiring@gmail.com>
2025-11-21 19:32:25 +0530

    Backend requirements built

--- server/src/controllers/links.controller.js
+++ server/src/controllers/links.controller.js
@@ -22,7 +22,7 @@ async function createLink(req, res) {
 async function getAllLinks(req, res) {
   try {
     const result = await pool.query(
-      "SELECT code, url, created_at FROM links ORDER BY id DESC"
+      "SELECT code, url, created_at,clicks,last_accessed FROM links ORDER BY id DESC"
     );
 
     return res.status(200).json(result.rows);
--- server/src/index.js
+++ server/src/index.js
@@ -21,18 +21,36 @@ app.get('/:code', async (req, res) => {
   try {
     const { code } = req.params;
 
+    console.log("Redirect route hit for:", code);
+
+    // 1. Fetch long URL
     const result = await pool.query(
       'SELECT url FROM links WHERE code = $1',
       [code]
     );
 
     if (result.rows.length === 0) {
+      console.log("Code not found:", code);
       return res.status(404).send('Not found');
     }
 
     const longUrl = result.rows[0].url;
 
+    //  Update analytics 
+    const update = await pool.query(
+      `UPDATE links
+       SET clicks = clicks + 1,
+           last_accessed = NOW()
+       WHERE code = $1
+       RETURNING clicks, last_accessed`,
+      [code]
+    );
+
+    console.log("Analytics updated:", update.rows[0]);
+
+    // 3. Redirect to long URL
     return res.redirect(302, longUrl);
+
   } catch (err) {
     console.error("REDIRECT ERROR:", err);
     return res.status(500).send('Server error');
